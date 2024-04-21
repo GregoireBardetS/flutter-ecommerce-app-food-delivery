7deff6ed4c1df09ec18c956eeddf056a8b7e4c85
GregoireBardetS <105701284+GregoireBardetS@users.noreply.github.com>
2024-04-21 17:36:46 +0400

    2eme heure

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,3 +1,5 @@
+import 'package:dots_indicator/dots_indicator.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery/utils/colors.dart';
 import 'package:food_delivery/widgets/big_text.dart';
@@ -16,7 +18,8 @@ class _FoodPageBodyState extends State<FoodPageBody> {
     viewportFraction: 0.85
   );
   var _currPageValue = 0.0;
-  double _scaleFactor = 0.8;
+  final double _scaleFactor = 0.8;
+  final double _height = 220;
 
   @override
   void initState(){
@@ -36,16 +39,32 @@ class _FoodPageBodyState extends State<FoodPageBody> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      //color: Colors.redAccent,
-      height: 320,
-      child: PageView.builder(
-        controller: pageController,
-        itemCount: 5,
-        itemBuilder: (context, index){
-          return _buildPageItem(index,);
-        }
-      ),
+    return Column(
+      children: [
+        Container(
+          //color: Colors.redAccent,
+          height: 320,
+          child: PageView.builder(
+            controller: pageController,
+            itemCount: 5,
+            itemBuilder: (context, index){
+              return _buildPageItem(index,);
+            }
+          ),
+        ),
+        Container(
+          child: new DotsIndicator(
+            dotsCount: 5,
+            position: _currPageValue,
+            decorator: DotsDecorator(
+              size: const Size.square(9.0),
+              activeSize: const Size(18.0, 9.0),
+              activeShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+              activeColor: AppColors.mainColor,
+            ),
+          )
+        )
+      ],
     );
   }
 
@@ -54,11 +73,27 @@ class _FoodPageBodyState extends State<FoodPageBody> {
     Matrix4 matrix = new Matrix4.identity();
     if (index==_currPageValue.floor()) {
       var currScale = 1 - (_currPageValue - index) * (1-_scaleFactor);
-      var currTrans = 120;
-      matrix = Matrix4.diagonal3Values(1, currScale, 1);
-    } else if (index==_currPageValue.floor()+1) {
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+    }
+    else if (index==_currPageValue.floor()+1) {
       var currScale = _scaleFactor + (_currPageValue-index+1) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
       matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+    }
+    else if (index==_currPageValue.floor()-1) {
+      var currScale = 1 - (_currPageValue - index) * (1-_scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+    }
+    else {
+      var currScale = 0.8;
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
+
     }
 
 
@@ -84,8 +119,23 @@ class _FoodPageBodyState extends State<FoodPageBody> {
               height: 120,
               margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                color: Colors.white
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0xFFe8e8e8),
+                    blurRadius: 5.0,
+                    offset: Offset(0, 5)
+                  ),
+                  BoxShadow(
+                    color: Colors.white,
+                    offset: Offset(-5,0)
+                  ),
+                  BoxShadow(
+                    color: Colors.white,
+                    offset: Offset(5,0)
+                  )
+                ]
               ),
               child: Container(
                 margin: const EdgeInsets.only(top:15, left: 15, right: 15, bottom: 15),
